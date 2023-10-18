8eb64f741bb293dadca4a3f3c73e7f2a95b0583b
vanhiupun <fanxiaobin422@gmail.com>
2023-10-19 00:21:39 +0800

    🎈 perf: 去除不需要的功能 优化访问体验

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,43 +1,9 @@
 // .vitepress/theme/index.ts */
-import { h, watch, onMounted, nextTick } from 'vue'
-import { useData, EnhanceAppContext, useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import mediumZoom from 'medium-zoom'
 import { inject } from '@vercel/analytics'
 
 inject()
 import './custom.css'
 import './custom-blocks.css'
-import './index.css'
 
-let homePageStyle: HTMLStyleElement | undefined
-
-export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    const props: Record<string, any> = {}
-    // 获取 frontmatter
-    const { frontmatter } = useData()
-
-    /* 添加自定义 class */
-    if (frontmatter.value?.layoutClass) {
-      props.class = frontmatter.value.layoutClass
-    }
-
-    return h(DefaultTheme.Layout, props)
-  },
-  setup() {
-    const route = useRoute()
-    const initZoom = () => {
-      mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' })
-      mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
-    }
-    onMounted(() => {
-      initZoom()
-    })
-    watch(
-      () => route.path,
-      () => nextTick(() => initZoom())
-    )
-  }
-}
+export default { extends: DefaultTheme }
