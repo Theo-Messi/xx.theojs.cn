59ea30b1c9d2533194dad162131a5cb9900c67fa
Theo <fanxiaobin422@gmail.com>
2023-10-19 12:22:22 +0800

    🌈 style: 添加注释 (#3)

--- .vitepress/configs/head.ts
+++ .vitepress/configs/head.ts
@@ -3,12 +3,17 @@ import type { HeadConfig } from 'vitepress'
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 export const head: HeadConfig[] = [
-  // LOGO
+  // 设置图标
   ['link', { rel: 'icon', href: 'https://theovan.xyz/avatar.jpg' }],
+  // 设置主题颜色
   ['meta', { name: 'theme-color', content: '#5f67ee' }],
+  // 设置OG类型
   ['meta', { name: 'og:type', content: 'website' }],
+  // 设置OG语言
   ['meta', { name: 'og:locale', content: 'cn' }],
-  ['meta', { name: 'og:site_name', content: 'Theo Notes' }],
+  // 设置OG站点名称
+  ['meta', { name: 'og:site_name', content: '玄学宝典' }],
+  // 设置OG图片
   ['meta', { name: 'og:image', content: 'https://theovan.xyz/avatar.png' }]
   // 强制开启为深色模式
   // ["script", {}, `document.documentElement.classList.add('dark')`],
