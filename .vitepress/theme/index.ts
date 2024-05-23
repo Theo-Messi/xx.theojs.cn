e85b532c67450414df699d10a08e7059a153a395
Theo <fanxiaobin422@gmail.com>
2024-05-24 01:13:54 +0800

    chore: add google analytics

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,16 @@
 // .vitepress/theme/index.ts */
 import DefaultTheme from 'vitepress/theme'
 import { inject } from '@vercel/analytics'
+import googleAnalytics from 'vitepress-plugin-google-analytics'
 
 // 注入分析
 inject()
 // 导入自定义样式
 import './styles/index.scss'
 // 导出默认主题
-export default { extends: DefaultTheme }
+export default {
+  extends: DefaultTheme,
+  enhanceApp: (ctx) => {
+    googleAnalytics({ id: 'G-5SHLV23EGQ' })
+  }
+}
