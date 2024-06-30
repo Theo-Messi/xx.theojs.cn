b268092d2d4211cca6fae34a8e20dacac178c7f5
Theo <fanxiaobin422@gmail.com>
2024-06-30 15:56:20 +0800

    faet!(style): page beautification

---
+++ .vitepress/theme/components/AsideLogo.vue
@@ -0,0 +1,145 @@
+<template>
+  <!-- <a class="banner mp" href="https://fbinv01.fbaff.cc/auth/register?code=RZP3" target="_blank">
+    <img
+      width="22"
+      height="22"
+      src="https://flyingbirdlimo.com/wp-content/uploads/2022/03/Flying-Bird-Logo-cropped.png"
+    />
+    <span>
+      <p class="Activity">FlyingBird 618活动来了</p>
+      <p class="extra">月付 85折 优惠码：2461885</p>
+      <p class="extra">年付 64折 优惠码：2461880</p>
+    </span>
+  </a> -->
+
+  <a class="banner mp" href="https://doc.theovan.cn/" target="_blank">
+    <img width="22" height="22" src="https://i.theovan.cn/avatar.png" />
+    <span>
+      <p class="extra-info">流媒体观影</p>
+      <p class="heading">Theo-Docs</p>
+      <p class="extra-info">一站式服务</p>
+    </span>
+  </a>
+  <a class="banner mp" href="https://share.theovan.cn/" target="_blank">
+    <img width="22" height="22" src="https://i.theovan.cn/docs/202405101119004.png" />
+    <span>
+      <p class="extra-info">快速获取热门影视资源</p>
+      <p class="heading">阿里云盘资源分享</p>
+      <p class="extra-info">转存观看!</p>
+    </span>
+  </a>
+  <a class="banner mp" href="https://doc.theovan.cn/serve/sharing/Account-sharing-guide" target="_blank">
+    <img width="22" height="22" src="https://cdn.iconscout.com/icon/free/png-256/free-netflix-2296042-1912001.png" />
+    <span>
+      <p class="extra-info">流媒体账号合租</p>
+      <p class="heading">流媒体帐号合租</p>
+      <p class="extra-info">共享车位</p>
+    </span>
+  </a>
+  <a class="banner mp" href="https://github.com/sponsors/Theo-messi" target="_blank">
+    <img width="22" height="22" src="https://doc.theovan.cn/爱心.png" />
+    <span>
+      <!-- <p class="extra-info">Sponsor for me</p> -->
+      <p class="heading">为我赞助</p>
+      <!-- <p class="extra-info">Sponsor now!</p> -->
+    </span>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.VPDocAsideSponsors {
+  margin-top: 8px !important;
+}
+
+:deep(.vp-sponsor-grid.mini .vp-sponsor-grid-image) {
+  max-width: 158px;
+  max-height: 48px;
+}
+
+:deep(.vp-sponsor-grid.xmini .vp-sponsor-grid-image) {
+  max-width: 80px;
+  max-height: 32px;
+}
+
+:deep(.vp-sponsor-grid.xmini img) {
+  transition: transform 0.5s;
+  transform: scale(1);
+}
+
+:deep(.vp-sponsor-grid.xmini:hover img) {
+  transform: scale(1.15);
+}
+
+.banner {
+  margin: 0.25rem 0;
+  padding: 0.4rem 0;
+  border-radius: 14px;
+  position: relative;
+  font-size: 0.9rem;
+  font-weight: 700;
+  line-height: 1.1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 1rem;
+  background-color: var(--vp-c-bg-alt);
+  border: 2px solid var(--vp-c-bg-alt);
+  transition: border-color 0.5s;
+
+  &:last-of-type {
+    margin-bottom: 1rem;
+  }
+
+  &:hover {
+    border: 2px solid var(--vp-c-brand-1);
+  }
+
+  img {
+    transition: transform 0.5s;
+    transform: scale(1.25);
+  }
+
+  &:hover img {
+    transform: scale(1.75);
+  }
+
+  .extra-info {
+    color: var(--vp-c-text-1);
+    opacity: 0;
+    font-size: 0.7rem;
+    padding-left: 0.1rem;
+    transition: opacity 0.5s;
+  }
+
+  .extra {
+    color: var(--vp-c-text-2);
+    font-size: 0.7rem;
+    padding-left: 0.1rem;
+  }
+
+  .heading {
+    background-image: linear-gradient(120deg, var(--vp-c-brand-3) 32%, var(--vp-c-brand-2), var(--vp-c-brand-1));
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  .Activity {
+    background-image: linear-gradient(120deg, #b047ff 16%, #646cff, #747bff);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  &:hover {
+    .extra-info {
+      opacity: 0.9;
+    }
+
+    .extra {
+      opacity: 0.9;
+    }
+  }
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,11 +3,14 @@ import DefaultTheme from 'vitepress/theme'
 import { inject } from '@vercel/analytics'
 import googleAnalytics from 'vitepress-plugin-google-analytics'
 import { h } from 'vue'
-import AsideSponsors from './components/pendant.vue'
+import Box from './components/Box.vue'
+import Links from './components/Links.vue'
+import AsideLogo from './components/AsideLogo.vue'
 import { NolebaseGitChangelogPlugin } from '@nolebase/vitepress-plugin-git-changelog/client'
 
-import './styles/index.css'
+import './styles/index.scss'
 import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 inject()
 
 // 导出默认主题
@@ -15,25 +18,27 @@ export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'aside-ads-before': () => h(AsideSponsors)
+      'aside-ads-before': () => h(AsideLogo)
     })
   },
   enhanceApp: ({ app }, ctx) => {
-    googleAnalytics({ id: 'G-5SHLV23EGQ' }),
-      app.use(NolebaseGitChangelogPlugin, {
-        mapAuthors: [
-          {
-            name: 'Theo',
-            username: 'Theo-messi',
-            mapByNameAliases: ['Theo', 'vanhiupun'],
-            mapByEmailAliases: ['fanxiaobin422@gmail.com', 'fanxiaobin422@sina.com', '771720405@qq.com']
-          },
-          {
-            name: 'dependabot',
-            username: 'dependabot',
-            mapByEmailAliases: ['49699333+dependabot[bot]@users.noreply.github.com']
-          }
-        ]
-      })
+    googleAnalytics({ id: 'G-5SHLV23EGQ' })
+    app.component('Box', Box)
+    app.component('Links', Links)
+    app.use(NolebaseGitChangelogPlugin, {
+      mapAuthors: [
+        {
+          name: 'Theo',
+          username: 'Theo-messi',
+          mapByNameAliases: ['Theo', 'vanhiupun'],
+          mapByEmailAliases: ['fanxiaobin422@gmail.com', 'fanxiaobin422@sina.com', '771720405@qq.com']
+        },
+        {
+          name: 'dependabot',
+          username: 'dependabot',
+          mapByEmailAliases: ['49699333+dependabot[bot]@users.noreply.github.com']
+        }
+      ]
+    })
   }
 }
