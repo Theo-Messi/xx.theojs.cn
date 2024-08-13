8fbaa3eab60b1fe12b3e6fa883dd81ffaf8b303b
Theo <fanxiaobin422@gmail.com>
2024-08-13 22:06:37 +0800

    docs: add new partner

---
+++ .vitepress/data/AsideData.ts
@@ -0,0 +1,49 @@
+export const Aside_Data = [
+  // {
+  //   link: 'https://fbinv01.fbaff.cc/auth/register?code=RZP3',
+  //   icon: 'https://flyingbirdlimo.com/wp-content/uploads/2022/03/Flying-Bird-Logo-cropped.png',
+  //   Activity: 'FlyingBird 618活动来了',
+  //   info1: '月付 85折 优惠码  2461885',
+  //   info2: '年付 64折 优惠码  2461880'
+  // },
+  {
+    link: 'https://ivt01.qytaff.cc/register?aff=jjgD79Jd',
+    icon: 'https://www.qingyunti.cc/images/qytlogo-144-49.png',
+    Activity: '青云梯·三周年专属优惠',
+    info1: '月付 85折 优惠码  qyt85',
+    info2: '季付以上8折 优惠码  qyt8'
+  },
+  {
+    link: 'https://github.com/sponsors/Theo-Messi',
+    icon: 'https://doc.theojs.cn/爱心.png',
+    name: '赞助我的项目'
+  },
+  {
+    link: 'https://doc.theojs.cn/',
+    icon: 'https://i.theojs.cn/avatar.png',
+    name: 'Theo-Docs',
+    hide1: '流媒体观影',
+    hide2: '一站式服务'
+  },
+  {
+    link: 'https://share.theojs.cn/',
+    icon: 'https://i.theojs.cn/docs/202405101119004.png',
+    name: '阿里云盘资源分享',
+    hide1: '快速获取热门影视资源',
+    hide2: '转存观看!'
+  },
+  {
+    link: 'https://doc.theojs.cn/serve/sharing/Account-sharing-guide',
+    icon: 'https://cdn.iconscout.com/icon/free/png-256/free-netflix-2296042-1912001.png',
+    name: '流媒体帐号合租',
+    hide1: '流媒体账号合租',
+    hide2: '共享车位'
+  },
+  {
+    link: 'https://github.com/Theo-Messi/dotfiles',
+    icon: 'https://img.icons8.com/?size=400&id=nuPce-GYYZeC&format=png&color=000000',
+    name: '常用配置 - dotfiles',
+    hide1: 'zshrc、系统优化一键脚本',
+    hide2: 'Clash/Loon/QX... 配置'
+  }
+]
