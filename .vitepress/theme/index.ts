39ea0de1afced2130a2396386fdb84185d1126ff
Theo <fanxiaobin422@gmail.com>
2024-08-26 10:58:06 +0800

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -13,14 +13,15 @@ import {
   HomeUnderline
 } from '@theojs/lumen'
 
-import { Aside_Data } from '../data/AsideData'
+import { Aside_Data, Footer_Data } from '../data'
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'home-hero-info-before': () => h(Announcement),
-      'aside-ads-before': () => h(DocAsideLogo, { Aside_Data })
+      'aside-ads-before': () => h(DocAsideLogo, { Aside_Data }),
+      'layout-bottom': () => h(HomeFooter, { Footer_Data })
     })
   },
   enhanceApp: ({ app }, ctx) => {
