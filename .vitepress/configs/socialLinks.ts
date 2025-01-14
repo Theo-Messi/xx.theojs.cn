e6f22f8b44fcdd2b3f1a764b3293d7176ba68472
Theo <fanxiaobin422@gmail.com>
2025-01-14 14:13:09 +0800

    chore: fix the broken links.

--- .vitepress/configs/socialLinks.ts
+++ .vitepress/configs/socialLinks.ts
@@ -5,7 +5,7 @@ import type { DefaultTheme } from 'vitepress'
 export const socialLinks: DefaultTheme.SocialLink[] = [
   {
     icon: 'github',
-    link: 'https://github.com/Theo-Messi/Theo-Docs',
+    link: 'https://github.com/Theo-Messi/xx.theojs.cn',
     ariaLabel: 'Github'
   },
   {
