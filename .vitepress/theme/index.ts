cd4d3a807f59938b5774490d0b4411a3c054a6e8
Theo <fanxiaobin422@gmail.com>
2025-05-18 17:55:15 +0800

    build: bump deps

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -12,12 +12,11 @@ import {
   HomeFooter,
   HomeUnderline,
   ShareButton,
-  Twikoo,
   umamiAnalytics
 } from '@theojs/lumen'
 import '@theojs/lumen/theme'
 
-import { Aside_Data, Footer_Data, Twikoo_Data } from '../data'
+import { Aside_Data, Footer_Data } from '../data'
 
 export default {
   extends: DefaultTheme,
@@ -25,7 +24,6 @@ export default {
     return h(DefaultTheme.Layout, null, {
       'aside-ads-before': () => h(DocAsideLogo, { Aside_Data }),
       'aside-outline-before': () => h(ShareButton),
-      'doc-after': () => h(Twikoo, { Twikoo_Data }),
       'home-hero-info-before': () => h(Announcement),
       'layout-bottom': () => h(HomeFooter, { Footer_Data })
     })
