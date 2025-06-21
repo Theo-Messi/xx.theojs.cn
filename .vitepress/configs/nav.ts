8109c49238cdb6267cb0c2905d3eb188c309bcf5
Theo <fanxiaobin422@gmail.com>
2025-06-21 09:54:04 +0800

    chore: update discount campaign content

--- .vitepress/configs/nav.ts
+++ .vitepress/configs/nav.ts
@@ -10,13 +10,6 @@ export const nav: DefaultTheme.NavItem[] = [
   { text: '卜', link: '卜/毕法赋/毕法赋-上', activeMatch: '^/卜/' },
   { text: '灵宠', link: '灵宠/宠物篇', activeMatch: '^/灵宠/' },
   { text: '相关经典', link: '相关经典/道德经', activeMatch: '^/相关经典/' },
-  {
-    text: '提交书籍与纠错',
-    link: '提交书籍与纠错',
-    activeMatch: '^/提交书籍与纠错'
-  },
-  {
-    text: '服务推荐',
-    link: 'https://doc.theojs.cn/serve/sharing/Account-sharing-guide'
-  }
+  { text: '提交书籍与纠错', link: '提交书籍与纠错', activeMatch: '^/提交书籍与纠错' },
+  { text: '服务推荐', link: 'https://doc.theojs.cn/serve/sharing/Account-sharing-guide' }
 ]
