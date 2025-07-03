6b2bb5bd0af1af8961723c86c42c0e39f2df16e7
Theo <fanxiaobin422@gmail.com>
2025-07-03 18:34:02 +0800

    chore: upgrade to latest @theojs/lumen and use new API

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,16 +3,7 @@ import DefaultTheme from 'vitepress/theme'
 
 import { h } from 'vue'
 
-import {
-  Announcement,
-  DocAsideLogo,
-  DocBoxCube,
-  DocLinks,
-  DocPill,
-  HomeFooter,
-  HomeUnderline,
-  umamiAnalytics
-} from '@theojs/lumen'
+import { Aside, BoxCube, Footer, Links, Notice, Pill, Underline, umamiAnalytics } from '@theojs/lumen'
 import '@theojs/lumen/style'
 
 import { Aside_Data, Footer_Data } from '../data'
@@ -21,9 +12,9 @@ export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'aside-ads-before': () => h(DocAsideLogo, { Aside_Data }),
-      'home-hero-info-before': () => h(Announcement),
-      'layout-bottom': () => h(HomeFooter, { Footer_Data })
+      'aside-ads-before': () => h(Aside, { Aside_Data }),
+      'home-hero-info-before': () => h(Notice),
+      'layout-bottom': () => h(Footer, { Footer_Data })
     })
   },
   enhanceApp: ({ app }) => {
@@ -32,9 +23,9 @@ export default {
       src: import.meta.env.VITE_UMAMI_SRC,
       domains: 'xx.theojs.cn'
     })
-    app.component('Home', HomeUnderline)
-    app.component('Pill', DocPill)
-    app.component('Links', DocLinks)
-    app.component('BoxCube', DocBoxCube)
+    app.component('Home', Underline)
+    app.component('Pill', Pill)
+    app.component('Links', Links)
+    app.component('BoxCube', BoxCube)
   }
 }
